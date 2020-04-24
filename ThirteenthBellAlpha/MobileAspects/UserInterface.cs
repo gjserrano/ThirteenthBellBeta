9d4f19caca4fb442ef5d6aefa9d5159e3e4c1d5d
Gabriel <gjserrano@ksu.edu>
2020-04-23 23:39:12 -0500

    Added Game Background and User Interface with Placeholder Text

---
+++ ThirteenthBellAlpha/MobileAspects/UserInterface.cs
@@ -0,0 +1,114 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ThirteenthBellAlpha.Components;
+
+namespace ThirteenthBellAlpha.MobileAspects
+{
+    class UserInterface : Component
+    {
+        Texture2D texture;
+
+        SpriteFont font;
+
+        Vector2 position { get; set; }
+
+        Rectangle Rectangle
+        {
+            get
+            {
+                return new Rectangle(0, 0, texture.Width, texture.Height);
+            }
+        }
+
+        Rectangle playerStackRectangle
+        {
+            get
+            {
+                return new Rectangle(3, 612, 115, 90);
+            }
+        }
+
+        Rectangle playerLifeRectangle
+        {
+            get
+            {
+                return new Rectangle(1252, 614, 115, 90);
+            }
+        }
+
+        Rectangle enemyStackRectangle
+        {
+            get
+            {
+                return new Rectangle(3, 47, 115, 90);
+            }
+        }
+
+        Rectangle enemyLifeRectangle
+        {
+            get
+            {
+                return new Rectangle(1252, 47, 115, 90);
+            }
+        }
+
+        public string playerStackText { get; set; }
+        public string playerLifeText { get; set; }
+
+        public string enemyStackText { get; set; }
+        public string enemyLifeText { get; set; }
+
+        public UserInterface(Texture2D textureUI, SpriteFont fontUI) 
+        {
+            texture = textureUI;
+            font = fontUI;
+        }
+
+        public override void Draw(GameTime gameTime, SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(texture, Rectangle, null, Color.White, 0f, new Vector2(0, 0), SpriteEffects.None, .98f);
+
+            if (!string.IsNullOrEmpty(playerStackText))
+            {
+                var x = (playerStackRectangle.X + (playerStackRectangle.Width / 2)) - (font.MeasureString(playerStackText).X / 2);
+                var y = (playerStackRectangle.Y + (playerStackRectangle.Height / 2)) - (font.MeasureString(playerStackText).Y / 2);
+
+                spriteBatch.DrawString(font, playerStackText, new Vector2(x, y), Color.Black);
+            }
+
+            if (!string.IsNullOrEmpty(playerLifeText))
+            {
+                var x = (playerLifeRectangle.X + (playerLifeRectangle.Width / 2)) - (font.MeasureString(playerLifeText).X / 2);
+                var y = (playerLifeRectangle.Y + (playerLifeRectangle.Height / 2)) - (font.MeasureString(playerLifeText).Y / 2);
+
+                spriteBatch.DrawString(font, playerLifeText, new Vector2(x, y), Color.Black);
+            }
+
+            if (!string.IsNullOrEmpty(enemyStackText))
+            {
+                var x = (enemyStackRectangle.X + (enemyStackRectangle.Width / 2)) - (font.MeasureString(enemyStackText).X / 2);
+                var y = (enemyStackRectangle.Y + (enemyStackRectangle.Height / 2)) - (font.MeasureString(enemyStackText).Y / 2);
+
+                spriteBatch.DrawString(font, enemyStackText, new Vector2(x, y), Color.Black);
+            }
+
+            if (!string.IsNullOrEmpty(enemyLifeText))
+            {
+                var x = (enemyLifeRectangle.X + (enemyLifeRectangle.Width / 2)) - (font.MeasureString(enemyLifeText).X / 2);
+                var y = (enemyLifeRectangle.Y + (enemyLifeRectangle.Height / 2)) - (font.MeasureString(enemyLifeText).Y / 2);
+
+                spriteBatch.DrawString(font, enemyLifeText, new Vector2(x, y), Color.Black);
+            }
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+
+        }
+    }
+}
--- ThirteenthBellAlpha/States/GameState.cs
+++ ThirteenthBellAlpha/States/GameState.cs
@@ -6,13 +6,36 @@ using System.Threading.Tasks;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Graphics;
+using ThirteenthBellAlpha.Components;
+using ThirteenthBellAlpha.MobileAspects;
 
 namespace ThirteenthBellAlpha.States
 {
     public class GameState : State
     {
+        private List<Component> _components;
+
         public GameState(Game1 game, GraphicsDevice graphicsDevice, ContentManager content) : base(game, graphicsDevice, content)
         {
+            var backgroundTexture = _content.Load<Texture2D>("Menu Backgrounds/Stone Brick Background");
+            Background background = new Background(backgroundTexture);
+
+            var uiFont = _content.Load<SpriteFont>("Fonts/Font");
+
+            var uiTexture = _content.Load<Texture2D>("Menu Backgrounds/ThirteenthBellUI_NoBackground");
+            UserInterface userInterface = new UserInterface(uiTexture, uiFont)
+            {
+                playerStackText = "Test Text", //Hopefully we'll be able to plug in the different player stats for these
+                playerLifeText = "Test Text",
+                enemyStackText = "Test Text",
+                enemyLifeText = "Test Text"
+            };
+
+            _components = new List<Component>
+            {
+                background,
+                userInterface,
+            };
         }
 
         public override void Draw(GameTime gameTime, SpriteBatch spriteBatch)
@@ -21,6 +44,11 @@ namespace ThirteenthBellAlpha.States
 
             //Start
             _graphicsDevice.Clear(Color.CornflowerBlue);
+
+            foreach(var component in _components)
+            {
+                component.Draw(gameTime, spriteBatch);
+            }
 
             spriteBatch.End();
         }
