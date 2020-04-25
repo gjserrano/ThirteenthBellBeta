3d1b25e20002712ae9412e4205dcd3328417b2b7
Gabriel <gjserrano@ksu.edu>
2020-04-25 16:08:08 -0500

    Added LaneTile, Lane, and LaneSet Classes

---
+++ ThirteenthBellAlpha/Components/Lane.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace ThirteenthBellAlpha.Components
+{
+    class Lane : Component
+    {
+        Texture2D topTexture;
+        Texture2D bottomTexture;
+
+        List<LaneTile> compiledLane;
+
+        public Lane(LaneTile top, List<LaneTile> laneTiles, LaneTile bottom, Texture2D tTexture, Texture2D bTexture)
+        {
+            topTexture = tTexture;
+            bottomTexture = bTexture;
+
+            top = new LaneTile(topTexture);
+            bottom = new LaneTile(bottomTexture);
+
+            compiledLane.Add(top);
+            compiledLane.AddRange(laneTiles);
+            compiledLane.Add(bottom);
+        }
+
+        public override void Draw(GameTime gameTime, SpriteBatch spriteBatch)
+        {
+            foreach(var lanePart in compiledLane)
+            {
+                lanePart.Draw(gameTime, spriteBatch);
+            }
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            //throw new NotImplementedException();
+        }
+    }
+}
---
+++ ThirteenthBellAlpha/Components/LaneTile.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThirteenthBellAlpha.Components
+{
+    class LaneTile : Component
+    {
+        Vector2 position { get; set; }
+
+        private Texture2D _texture;
+
+        Rectangle Rectangle
+        {
+            get
+            {
+                return new Rectangle(0, 0, _texture.Width, _texture.Height);
+            }
+        }
+
+        public LaneTile(Texture2D texture)
+        {
+            _texture = texture;
+        }
+
+        public override void Draw(GameTime gameTime, SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(_texture, Rectangle, null, Color.White, 0f, new Vector2(0, 0), SpriteEffects.None, .98f); ;
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+
+        }
+    }
+}
