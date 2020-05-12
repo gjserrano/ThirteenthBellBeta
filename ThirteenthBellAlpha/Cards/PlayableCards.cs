0db23925796d25ca10e70517b32dd7dab5f25d85
Gabriel <gjserrano@ksu.edu>
2020-05-11 23:47:03 -0500

    Added CardSlot Disabling for Enemy Player

--- ThirteenthBellAlpha/Cards/PlayableCards.cs
+++ ThirteenthBellAlpha/Cards/PlayableCards.cs
@@ -30,6 +30,16 @@ namespace ThirteenthBellAlpha.Cards
         public PlayerNorth enemyPlayer;
 
         bool playZ = true;
+        bool playX = true;
+        bool playC = true;
+        bool playV = true;
+        bool playB = true;
+
+        bool playQ = true;
+        bool playW = true;
+        bool playE = true;
+        bool playR = true;
+        bool playT = true;
 
         int sideID;
 
@@ -93,36 +103,72 @@ namespace ThirteenthBellAlpha.Cards
 
                 }
 
-                else if (_currentKeyboardState.IsKeyDown(Keys.X) && _previousKeyboardState.IsKeyUp(Keys.X) && playerStack.stack.Count > 0)
+                else if (_currentKeyboardState.IsKeyDown(Keys.X) && _previousKeyboardState.IsKeyUp(Keys.X) && playX)
                 {
-                    _projectiles.Add(new BasicRotating(hand[1].card.projectileTexture, .15f, hand[1].card.projectileSpeed, hand[1].card.projectileDamage, (int)player.Bounds.X + 20, (int)player.Bounds.Y + 20));
-                    Card holder = playerStack.stack.Peek();
-                    playerStack.stack.Dequeue();
-                    hand[1] = new CardSlot(1, holder, 0);
+                    if (playerStack.stack.Count > 0)
+                    {
+                        _projectiles.Add(new BasicRotating(hand[1].card.projectileTexture, .15f, hand[1].card.projectileSpeed, hand[1].card.projectileDamage, (int)player.Bounds.X + 20, (int)player.Bounds.Y + 20));
+                        Card holder = playerStack.stack.Peek();
+                        playerStack.stack.Dequeue();
+                        hand[1] = new CardSlot(1, holder, 0);
+                    }
+                    else
+                    {
+                        _projectiles.Add(new BasicRotating(hand[1].card.projectileTexture, .15f, hand[1].card.projectileSpeed, hand[1].card.projectileDamage, (int)player.Bounds.X + 20, (int)player.Bounds.Y + 20));
+                        playX = false;
+                        hand[1].visible = false;
+                    }
                 }
 
-                else if (_currentKeyboardState.IsKeyDown(Keys.C) && _previousKeyboardState.IsKeyUp(Keys.C) && playerStack.stack.Count > 0)
+                else if (_currentKeyboardState.IsKeyDown(Keys.C) && _previousKeyboardState.IsKeyUp(Keys.C) && playC)
                 {
-                    _projectiles.Add(new BasicRotating(hand[2].card.projectileTexture, .15f, hand[2].card.projectileSpeed, hand[2].card.projectileDamage, (int)player.Bounds.X + 20, (int)player.Bounds.Y + 20));
-                    Card holder = playerStack.stack.Peek();
-                    playerStack.stack.Dequeue();
-                    hand[2] = new CardSlot(2, holder, 0);
+                    if (playerStack.stack.Count > 0)
+                    {
+                        _projectiles.Add(new BasicRotating(hand[2].card.projectileTexture, .15f, hand[2].card.projectileSpeed, hand[2].card.projectileDamage, (int)player.Bounds.X + 20, (int)player.Bounds.Y + 20));
+                        Card holder = playerStack.stack.Peek();
+                        playerStack.stack.Dequeue();
+                        hand[2] = new CardSlot(2, holder, 0);
+                    }
+                    else
+                    {
+                        _projectiles.Add(new BasicRotating(hand[2].card.projectileTexture, .15f, hand[2].card.projectileSpeed, hand[2].card.projectileDamage, (int)player.Bounds.X + 20, (int)player.Bounds.Y + 20));
+                        playC = false;
+                        hand[2].visible = false;
+                    }
                 }
 
-                else if (_currentKeyboardState.IsKeyDown(Keys.V) && _previousKeyboardState.IsKeyUp(Keys.V) && playerStack.stack.Count > 0)
+                else if (_currentKeyboardState.IsKeyDown(Keys.V) && _previousKeyboardState.IsKeyUp(Keys.V) && playV)
                 {
-                    _projectiles.Add(new BasicRotating(hand[3].card.projectileTexture, .15f, hand[3].card.projectileSpeed, hand[3].card.projectileDamage, (int)player.Bounds.X + 20, (int)player.Bounds.Y + 20));
-                    Card holder = playerStack.stack.Peek();
-                    playerStack.stack.Dequeue();
-                    hand[3] = new CardSlot(3, holder, 0);
+                    if (playerStack.stack.Count > 0)
+                    {
+                        _projectiles.Add(new BasicRotating(hand[3].card.projectileTexture, .15f, hand[3].card.projectileSpeed, hand[3].card.projectileDamage, (int)player.Bounds.X + 20, (int)player.Bounds.Y + 20));
+                        Card holder = playerStack.stack.Peek();
+                        playerStack.stack.Dequeue();
+                        hand[3] = new CardSlot(3, holder, 0);
+                    }
+                    else
+                    {
+                        _projectiles.Add(new BasicRotating(hand[3].card.projectileTexture, .15f, hand[3].card.projectileSpeed, hand[3].card.projectileDamage, (int)player.Bounds.X + 20, (int)player.Bounds.Y + 20));
+                        playV = false;
+                        hand[3].visible = false;
+                    }
                 }
 
-                else if (_currentKeyboardState.IsKeyDown(Keys.B) && _previousKeyboardState.IsKeyUp(Keys.B) && playerStack.stack.Count > 0)
+                else if (_currentKeyboardState.IsKeyDown(Keys.B) && _previousKeyboardState.IsKeyUp(Keys.B) && playB)
                 {
-                    _projectiles.Add(new BasicRotating(hand[4].card.projectileTexture, .15f, hand[4].card.projectileSpeed, hand[4].card.projectileDamage, (int)player.Bounds.X + 20, (int)player.Bounds.Y + 20));
-                    Card holder = playerStack.stack.Peek();
-                    playerStack.stack.Dequeue();
-                    hand[4] = new CardSlot(4, holder, 0);
+                    if (playerStack.stack.Count > 0)
+                    {
+                        _projectiles.Add(new BasicRotating(hand[4].card.projectileTexture, .15f, hand[4].card.projectileSpeed, hand[4].card.projectileDamage, (int)player.Bounds.X + 20, (int)player.Bounds.Y + 20));
+                        Card holder = playerStack.stack.Peek();
+                        playerStack.stack.Dequeue();
+                        hand[4] = new CardSlot(4, holder, 0);
+                    }
+                    else
+                    {
+                        _projectiles.Add(new BasicRotating(hand[4].card.projectileTexture, .15f, hand[4].card.projectileSpeed, hand[4].card.projectileDamage, (int)player.Bounds.X + 20, (int)player.Bounds.Y + 20));
+                        playB = false;
+                        hand[4].visible = false;
+                    }
                 }
 
                 foreach (var ammo in _projectiles)
@@ -133,44 +179,89 @@ namespace ThirteenthBellAlpha.Cards
 
             if(sideID == 1)
             {
-                if (_currentKeyboardState.IsKeyDown(Keys.Q) && _previousKeyboardState.IsKeyUp(Keys.Q) && enemyStack.stack.Count > 0)
+                if (_currentKeyboardState.IsKeyDown(Keys.Q) && _previousKeyboardState.IsKeyUp(Keys.Q) && playQ)
                 {
-                    _enemyProjectiles.Add(new BasicRotating(enemyHand[0].card.projectileTexture, .15f, enemyHand[0].card.projectileSpeed, enemyHand[0].card.projectileDamage, (int)enemyPlayer.Bounds.X + 20, (int)enemyPlayer.Bounds.Y + 20));
-                    Card holder = enemyStack.stack.Peek();
-                    enemyStack.stack.Dequeue();
-                    enemyHand[0] = new CardSlot(0, holder, 1);
+                    if (enemyStack.stack.Count > 0)
+                    {
+                        _enemyProjectiles.Add(new BasicRotating(enemyHand[0].card.projectileTexture, .15f, enemyHand[0].card.projectileSpeed, enemyHand[0].card.projectileDamage, (int)enemyPlayer.Bounds.X + 20, (int)enemyPlayer.Bounds.Y + 20));
+                        Card holder = enemyStack.stack.Peek();
+                        enemyStack.stack.Dequeue();
+                        enemyHand[0] = new CardSlot(0, holder, 1);
+                    }
+                    else
+                    {
+                        _enemyProjectiles.Add(new BasicRotating(enemyHand[0].card.projectileTexture, .15f, enemyHand[0].card.projectileSpeed, enemyHand[0].card.projectileDamage, (int)enemyPlayer.Bounds.X + 20, (int)enemyPlayer.Bounds.Y + 20));
+                        playQ = false;
+                        enemyHand[0].visible = false;
+                    }
                 }
 
-                else if (_currentKeyboardState.IsKeyDown(Keys.W) && _previousKeyboardState.IsKeyUp(Keys.W) && enemyStack.stack.Count > 0)
+                else if (_currentKeyboardState.IsKeyDown(Keys.W) && _previousKeyboardState.IsKeyUp(Keys.W) && playW)
                 {
-                    _enemyProjectiles.Add(new BasicRotating(enemyHand[1].card.projectileTexture, .15f, enemyHand[1].card.projectileSpeed, enemyHand[1].card.projectileDamage, (int)enemyPlayer.Bounds.X + 20, (int)enemyPlayer.Bounds.Y + 20));
-                    Card holder = enemyStack.stack.Peek();
-                    enemyStack.stack.Dequeue();
-                    enemyHand[1] = new CardSlot(1, holder, 1);
+                    if (enemyStack.stack.Count > 0)
+                    {
+                        _enemyProjectiles.Add(new BasicRotating(enemyHand[1].card.projectileTexture, .15f, enemyHand[1].card.projectileSpeed, enemyHand[1].card.projectileDamage, (int)enemyPlayer.Bounds.X + 20, (int)enemyPlayer.Bounds.Y + 20));
+                        Card holder = enemyStack.stack.Peek();
+                        enemyStack.stack.Dequeue();
+                        enemyHand[1] = new CardSlot(1, holder, 1);
+                    }
+                    else
+                    {
+                        _enemyProjectiles.Add(new BasicRotating(enemyHand[1].card.projectileTexture, .15f, enemyHand[1].card.projectileSpeed, enemyHand[1].card.projectileDamage, (int)enemyPlayer.Bounds.X + 20, (int)enemyPlayer.Bounds.Y + 20));
+                        playW = false;
+                        enemyHand[1].visible = false;
+                    }
                 }
 
-                else if (_currentKeyboardState.IsKeyDown(Keys.E) && _previousKeyboardState.IsKeyUp(Keys.E) && enemyStack.stack.Count > 0)
+                else if (_currentKeyboardState.IsKeyDown(Keys.E) && _previousKeyboardState.IsKeyUp(Keys.E) && playE)
                 {
-                    _enemyProjectiles.Add(new BasicRotating(enemyHand[2].card.projectileTexture, .15f, enemyHand[2].card.projectileSpeed, enemyHand[2].card.projectileDamage, (int)enemyPlayer.Bounds.X + 20, (int)enemyPlayer.Bounds.Y + 20));
-                    Card holder = enemyStack.stack.Peek();
-                    enemyStack.stack.Dequeue();
-                    enemyHand[2] = new CardSlot(2, holder, 1);
+                    if (enemyStack.stack.Count > 0)
+                    {
+                        _enemyProjectiles.Add(new BasicRotating(enemyHand[2].card.projectileTexture, .15f, enemyHand[2].card.projectileSpeed, enemyHand[2].card.projectileDamage, (int)enemyPlayer.Bounds.X + 20, (int)enemyPlayer.Bounds.Y + 20));
+                        Card holder = enemyStack.stack.Peek();
+                        enemyStack.stack.Dequeue();
+                        enemyHand[2] = new CardSlot(2, holder, 1);
+                    }
+                    else
+                    {
+                        _enemyProjectiles.Add(new BasicRotating(enemyHand[2].card.projectileTexture, .15f, enemyHand[2].card.projectileSpeed, enemyHand[2].card.projectileDamage, (int)enemyPlayer.Bounds.X + 20, (int)enemyPlayer.Bounds.Y + 20));
+                        playE = false;
+                        enemyHand[2].visible = false;
+                    }
                 }
 
-                else if (_currentKeyboardState.IsKeyDown(Keys.R) && _previousKeyboardState.IsKeyUp(Keys.R) && enemyStack.stack.Count > 0)
+                else if (_currentKeyboardState.IsKeyDown(Keys.R) && _previousKeyboardState.IsKeyUp(Keys.R) && playR)
                 {
-                    _enemyProjectiles.Add(new BasicRotating(enemyHand[3].card.projectileTexture, .15f, enemyHand[3].card.projectileSpeed, enemyHand[3].card.projectileDamage, (int)enemyPlayer.Bounds.X + 20, (int)enemyPlayer.Bounds.Y + 20));
-                    Card holder = enemyStack.stack.Peek();
-                    enemyStack.stack.Dequeue();
-                    enemyHand[3] = new CardSlot(3, holder, 1);
+                    if (enemyStack.stack.Count > 0)
+                    {
+                        _enemyProjectiles.Add(new BasicRotating(enemyHand[3].card.projectileTexture, .15f, enemyHand[3].card.projectileSpeed, enemyHand[3].card.projectileDamage, (int)enemyPlayer.Bounds.X + 20, (int)enemyPlayer.Bounds.Y + 20));
+                        Card holder = enemyStack.stack.Peek();
+                        enemyStack.stack.Dequeue();
+                        enemyHand[3] = new CardSlot(3, holder, 1);
+                    }
+                    else
+                    {
+                        _enemyProjectiles.Add(new BasicRotating(enemyHand[3].card.projectileTexture, .15f, enemyHand[3].card.projectileSpeed, enemyHand[3].card.projectileDamage, (int)enemyPlayer.Bounds.X + 20, (int)enemyPlayer.Bounds.Y + 20));
+                        playR = false;
+                        enemyHand[3].visible = false;
+                    }
                 }
 
-                else if (_currentKeyboardState.IsKeyDown(Keys.T) && _previousKeyboardState.IsKeyUp(Keys.T) && enemyStack.stack.Count > 0)
+                else if (_currentKeyboardState.IsKeyDown(Keys.T) && _previousKeyboardState.IsKeyUp(Keys.T) && playT)
                 {
-                    _enemyProjectiles.Add(new BasicRotating(enemyHand[4].card.projectileTexture, .15f, enemyHand[4].card.projectileSpeed, enemyHand[4].card.projectileDamage, (int)enemyPlayer.Bounds.X + 20, (int)enemyPlayer.Bounds.Y + 20));
-                    Card holder = enemyStack.stack.Peek();
-                    enemyStack.stack.Dequeue();
-                    enemyHand[4] = new CardSlot(4, holder, 1);
+                    if (enemyStack.stack.Count > 0)
+                    {
+                        _enemyProjectiles.Add(new BasicRotating(enemyHand[4].card.projectileTexture, .15f, enemyHand[4].card.projectileSpeed, enemyHand[4].card.projectileDamage, (int)enemyPlayer.Bounds.X + 20, (int)enemyPlayer.Bounds.Y + 20));
+                        Card holder = enemyStack.stack.Peek();
+                        enemyStack.stack.Dequeue();
+                        enemyHand[4] = new CardSlot(4, holder, 1);
+                    }
+                    else
+                    {
+                        _enemyProjectiles.Add(new BasicRotating(enemyHand[4].card.projectileTexture, .15f, enemyHand[4].card.projectileSpeed, enemyHand[4].card.projectileDamage, (int)enemyPlayer.Bounds.X + 20, (int)enemyPlayer.Bounds.Y + 20));
+                        playT = false;
+                        enemyHand[4].visible = false;
+                    }
                 }
 
                 foreach (var ammo in _enemyProjectiles)
